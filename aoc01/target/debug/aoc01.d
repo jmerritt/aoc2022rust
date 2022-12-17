1e09b770ec6aeb5ba6b9365ca8d47512a8d3b660
jmerritt <jay.merritt@gmail.com>
2022-12-17 13:20:36 -0500

    remove target dir

--- aoc01/target/debug/aoc01.d
+++
@@ -1,1 +0,0 @@
-/home/jmerritt/projects/aoc2022_rust/aoc01/target/debug/aoc01: /home/jmerritt/projects/aoc2022_rust/aoc01/src/main.rs
--- aoc01/target/debug/deps/aoc01-71ff93b02ded68db.d
+++
@@ -1,5 +0,0 @@
-/home/jmerritt/projects/aoc2022_rust/aoc01/target/debug/deps/aoc01-71ff93b02ded68db.rmeta: src/main.rs
-
-/home/jmerritt/projects/aoc2022_rust/aoc01/target/debug/deps/aoc01-71ff93b02ded68db.d: src/main.rs
-
-src/main.rs:
