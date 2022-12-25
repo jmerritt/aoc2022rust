78cf4a129a8cb7714c35908b6ce28ff1222c6cf9
jmerritt <jay.merritt@gmail.com>
2022-12-25 17:07:12 -0500

    aoc3 via tests

--- aoc03/src/lib.rs
+++ aoc03/src/lib.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use itertools::Itertools;
 
 pub fn day3part1(input: &str) -> u32 {
     let input = fs::read_to_string(input).unwrap();
@@ -18,12 +19,35 @@ pub fn day3part1(input: &str) -> u32 {
         .sum()
 }
 
+pub fn day3part2(input: &str) -> u32 {
+    let input = fs::read_to_string(input).unwrap();
+    input
+        .lines()
+        .map(|line| line.as_bytes())
+        .tuples()
+        .filter_map(|(bag1, bag2, bag3)| {
+            bag1
+                .iter()
+                .find(|item| bag2.contains(item) && bag3.contains(item))
+                .map(|item| match item {
+                    b'a'..=b'z' => (item - b'a') + 1,
+                    _ => (item - b'A') + 1 + 26,
+                } as u32)
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn trypart1() {
-        assert_eq!(day3part1("test.txt"), 3);
+        assert_eq!(day3part1("test.txt"), 36);
+    }
+
+    #[test]
+    fn trypart2() {
+        assert_eq!(day3part2("test.txt"), 4);
     }
 }
--- aoc03/src/main.rs
+++ aoc03/src/main.rs
@@ -1,6 +1,10 @@
 use aoc03::day3part1;
+use aoc03::day3part2;
 
 fn main() {
     let part_one_result = day3part1("input.txt");
     println!("{:#?}", part_one_result);
+
+    let part_two_result = day3part2("input.txt");
+    println!("{:#?}", part_two_result);
 }
